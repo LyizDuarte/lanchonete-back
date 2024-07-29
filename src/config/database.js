213aaa2b39e588f531fd3e86216aa7c178b2bea7
Luiz Duarte <looduarte21@gmail.com>
2024-07-29 15:57:53 -0300

    Adicionado config inicial do sequelize, rotas e etc

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'postgres',
+  database: 'lanchonete',
+  define: {
+    timespamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
---
+++ src/server.js
@@ -0,0 +1,3 @@
+import app from './app';
+
+app.listen(3000, console.log('O servidor iniciou na porta 3000'));
